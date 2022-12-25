02954520306b3caed1f4bbc3a6c5f4625d114794
Harryguci <huyquang0028@gmail.com>
2022-12-25 21:21:17 +0700

    fix some small bugs

--- public/javascript/copybutton.js
+++ public/javascript/copybutton.js
@@ -4,10 +4,10 @@ $(".copyButton").click(function () {
   var copyText = $(this);
   console.log(copyText.val());
   copyText.select();
-  //   copyText.setSelectionRange(0, 99999); // For mobile devices
+  // copyText.setSelectionRange(0, 99999); // For mobile devices // Why did this don't work?
 
   navigator.clipboard.writeText(copyText.val());
 
   // alert("Copied the text: " + copyText.val());
-  notificationHandler("Message", `Copied the text ${copyText.val()}`);
+  notificationHandler("Message", `Copied the text "${copyText.val()}"`);
 });
